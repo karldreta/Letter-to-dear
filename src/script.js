f284a02f603f3efae681410f8bc56c4ec7d148f5
Karl James <karldreta@gmail.com>
2024-12-29 20:33:07 +0800

    Initialize skeleton structure

--- src/script.js
+++ src/script.js
@@ -1,1 +1,16 @@
 import "./styles.css";
+
+const noButton = document.getElementById("no");
+noButton.addEventListener("mouseover", () => {
+  const randomX = Math.random() * (window.innerWidth - 100);
+  const randomY = Math.random() * (window.innerHeight - 50);
+  noButton.style.position = "absolute";
+  noButton.style.left = `${randomX}px`;
+  noButton.style.top = `${randomY}px`;
+});
+
+// document.getElementById("yes").addEventListener("click", () => {
+//   const pop = document.createElement('div');
+//   pop.innerHTML = `Thank you!`;
+//     pop.style.display = 'block';
+// });
